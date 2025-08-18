56a233060a1118db7245dae950355a5b2b966c49
Gennady Kaminsky <gennady@theleaguestock.com>
2025-08-18 16:37:33 -0500

    Initial Commit

---
+++ testAppliction/testApplictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApplictionApp.swift
+//  testAppliction
+//
+//  Created by Gennady Kaminsky on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct testApplictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
